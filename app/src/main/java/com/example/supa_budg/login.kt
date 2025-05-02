d69950f9f4c5f6eaa4a403ae2690c34bbc86373b
Umolemu <lambvries@gmail.com>
2025-05-02 21:21:42 +0200

    Add login page

---
+++ app/src/main/java/com/example/supa_budg/login.kt
@@ -0,0 +1,13 @@
+package com.example.supa_budg
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class Login: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
