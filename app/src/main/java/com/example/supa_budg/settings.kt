5ba7fcdef7d6059534c7e071caaf1ccda4d0cc42
Umolemu <lambvries@gmail.com>
2025-06-09 10:57:10 +0200

    Allow back in settings
    (cherry picked from commit f0ecb5ed08dc0c004cb1ce2a551c871e909103fa)

--- app/src/main/java/com/example/supa_budg/settings.kt
+++ app/src/main/java/com/example/supa_budg/settings.kt
@@ -2,6 +2,7 @@ package com.example.supa_budg
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,14 +14,17 @@ class Settings : AppCompatActivity() {
         setContentView(R.layout.settings)
 
         val exitSection = findViewById<LinearLayout>(R.id.exitSection)
-
         exitSection.setOnClickListener {
             Toast.makeText(this, "Signed out successfully", Toast.LENGTH_SHORT).show()
-
             val intent = Intent(this, Login::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
             finish()
+        }
+
+        val closeButton = findViewById<ImageView>(R.id.closeButton)
+        closeButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 }
